c774fd1809f8b2917bee846585a1f7d634c082c3
Dan Self <danielvmself@gmail.com>
2022-02-26 20:35:12 +1000

    Settings file updates

--- ANsite_DO/ANsite_DO/settings.py
+++ ANsite_DO/ANsite_DO/settings.py
@@ -139,7 +139,7 @@ EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
-EMAIL_BACKEND = django.core.mail.backends.smtp.EmailBackend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
